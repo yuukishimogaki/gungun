99ece2373e8aac59b97a19bae9e509951216f62c
nakazawaken1 <nakazawaken1@gmail.com>
2021-12-02 09:47:37 +0000

    add url

--- server/api/test.ts
+++ server/api/test.ts
@@ -4,6 +4,7 @@ export default (request: IncomingMessage, response: ServerResponse) => ({
   version: request.httpVersion,
   method: request.method,
   headers: request.headers,
+  url: request.url,
   params: Object.fromEntries(new URL(request.url!, `http://${request.headers.host}`).searchParams.entries()),
   body: request.read()?.toString()
 })
