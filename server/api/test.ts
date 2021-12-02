697aff65874b52da9a3bcb82fe8b84c325714e98
nakazawaken1 <nakazawaken1@gmail.com>
2021-12-02 09:38:29 +0000

    server api sample

---
+++ server/api/test.ts
@@ -0,0 +1,9 @@
+import type { IncomingMessage, ServerResponse } from 'http'
+
+export default (request: IncomingMessage, response: ServerResponse) => ({
+  version: request.httpVersion,
+  method: request.method,
+  headers: request.headers,
+  params: Object.fromEntries(new URL(request.url!, `http://${request.headers.host}`).searchParams.entries()),
+  body: request.read()?.toString()
+})
